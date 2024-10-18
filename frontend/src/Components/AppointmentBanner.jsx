bb067bec7431a40c0cb4f9aed99cb5c947539b9c
Fahad Khan Langah <fahadkhanavoid@gmail.com>
2024-10-18 19:13:58 +0500

    added React Typed

--- frontend/src/Components/AppointmentBanner.jsx
+++ frontend/src/Components/AppointmentBanner.jsx
@@ -1,18 +1,32 @@
+import { useState } from 'react';
 import girl from '../assets/appoint.png'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { ReactTyped } from 'react-typed';
 const AppointmentBanner = () => {
+  const data = [
+    "Book Appointment, With 100+ Available Doctors"
+  ]
+  const [key,setKey]= useState(0);
+  const handleComplete = () => {
+    setTimeout(() => {
+      setKey(key + 1);
+    }, 5000);
+  }
 
-  return (
-    <div className="bg-blue-600 overflow-hidden max-h-52 sm:max-h-72 rounded-md flex my-4 justify-center items-center">
-      <div className="sm:w-[60%] w-[70%] text-center px-10 text-white">
-        <h1 className="sm:text-4xl text-xl font-medium">Book Appointment, <br /> With 100+ Available Doctors</h1>
-        <Link to={'/login'}><button className="bg-white w-full px-5 text-black mt-4">Create Acount</button></Link>
+    return (
+      <div className="bg-blue-600 overflow-hidden max-h-52 sm:max-h-72 rounded-md flex my-4 justify-center items-center">
+        <div className="sm:w-[60%] w-[70%] text-center px-10 text-white">
+          <h1 className="sm:text-4xl text-xl font-medium">
+            {/* Book Appointment, <br /> With 100+ Available Doctors */}
+            <ReactTyped backDelay={500} key={key} strings={data} typeSpeed={80} backSpeed={20} onComplete={handleComplete} loop={false}></ReactTyped>
+          </h1>
+          <Link to={'/login'}><button className="bg-white w-full px-5 text-black mt-4">Create Acount</button></Link>
+        </div>
+        <div className='sm:w-[40%] w-[30%] flex justify-end'>
+          <img className='sm:h-[380px] h-[300px] w-full mt-16 sm:mt-12 object-fill sm:px-10' src={girl} alt="" />
+        </div>
       </div>
-      <div className='sm:w-[40%] w-[30%] flex justify-end'>
-        <img className='sm:h-[380px] h-[300px] w-full mt-16 sm:mt-12 object-fill sm:px-10' src={girl} alt="" />
-      </div>
-    </div>
-  )
-}
+    )
+  }
 
-export default AppointmentBanner
+  export default AppointmentBanner
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,7 +1,11 @@
 import docImg from '../assets/doctor.jfif'
 import docWithoutbg from '../assets/bg.png'
+import { ReactTyped } from 'react-typed'
 
 const Header = () => {
+  const data = [
+    "Browse our Trusted Doctors, <br />schedule your appointed hassle-free"
+  ]
   return (
     <div className="bg-blue-600 text-white flex justify-between h-[230px] sm:h-[320px]">
       <div className='flex justify-center items-center w-2/3 sm:w-[50%]'>
@@ -14,7 +18,10 @@ const Header = () => {
               <img className='w-10 h-10 rounded-full object-cover' src={docImg} alt="" />
               <img className='w-10 h-10 rounded-full object-cover -ml-2' src={docImg} alt="" />
             </div>
-            <p className='text-black font-medium text-sm'>Browse our Trusted Doctors, <br />schedule your appointed hassle-free</p>
+            <p className='text-black font-medium text-sm text-white'>
+              {/* Browse our Trusted Doctors, <br />schedule your appointed hassle-free */}
+              <ReactTyped backDelay={500} strings={data} typeSpeed={100} backSpeed={20} loop></ReactTyped>
+            </p>
           </div>
           <button>Book Appointment</button>
         </div>
